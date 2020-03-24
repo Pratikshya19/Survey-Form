492d828e10e5746b5773eb0db1cb5e3b0e79a352
github username <pratikshya.pp19@gmail.com>
2020-03-24 14:48:34 -0500

    message

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function back() {
+var firstname = document.forms["surveyform"]["FirstName"].value; 
+if (firstname == ""){
+    alert("Name must be filled out");
+    return false;
+}
+}                          
+
+
+
+
