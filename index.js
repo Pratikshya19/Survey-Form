4dd0ca5ef6ac68d0f2200e7e8a1544d9bd1680df
github username <pratikshya.pp19@gmail.com>
2020-03-24 17:18:52 -0500

    Survey Form

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 
-function back() {
-var firstname = document.forms["surveyform"]["FirstName"].value; 
-if (firstname == ""){
-    alert("Name must be filled out");
-    return false;
-}
-}                          
+function validateForm() {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
+    var phoneRe = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
 
+    if( document.surveyform.FirstName.value == "" ) {
+       alert( "Please provide your first name!" );
+       document.surveyform.FirstName.focus() ;
+       return false;
+    }
 
+    if( document.surveyform.Lastname.value == "" ) {
+        alert( "Please provide your last name!" );
+        document.surveyform.Lastname.focus() ;
+        return false;
+     }
 
+     if( document.surveyform.EMail.value.match(mailformat)) {
+        document.surveyform.EMail.focus() ;
+        return true;
+     }
+     else {
+         alert("You have entered an invalid email address!");
+         document.surveyform.EMail.focus() ;
+         return false
+     }
+
+     if(document.surveyform.Phone.valuematch(phoneRe)){
+document.surveyform.phone.focus();
+return true;
+     }
+     else {
+         alert("You have entered an invalid phone number")
+         document.surveyform.phone.focus();
+         return false
+     }
+    return( true );
+ }
